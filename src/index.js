3b763c08605703e30790c0c197cdcd9c02b8b0cd
wnkinc <140204064+wnkinc@users.noreply.github.com>
2024-08-29 12:35:15 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import 'normalize.css'; // Importing normalize.css from node_modules
 
 import "./styles.css";
-import { createTodoFromForm } from './formUI.js';
+import { createTodoFromForm } from './callConstructor.js';
 
 // Add event listener to the form
 document.getElementById('todoForm').addEventListener('submit', createTodoFromForm);
